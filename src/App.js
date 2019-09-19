38e0691b70698750a060ede974dcdd898c5d2da2
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-09-20 01:32:24 +0530

    Initialize notification library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Dashboard from './components/dashboard/Dashboard';
+import ReactNotification from 'react-notifications-component'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ReactNotification />
       <Dashboard></Dashboard>
     </div>
   );
