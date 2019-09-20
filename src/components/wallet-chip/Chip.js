1c34d5fe7a84cc8a159bc6d049024b27d939e54f
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-09-20 19:53:23 +0530

    minor changes in styling

--- src/components/wallet-chip/Chip.js
+++ src/components/wallet-chip/Chip.js
@@ -10,7 +10,7 @@ class Chip extends Component {
         </div>
         <div className="chip-info">
           <div className="currency-name">{this.props.currency}</div>
-          <div className="balance">Available: {this.props.balance}</div>
+          <div className="balance">Available: ${this.props.balance}</div>
         </div>
       </div>
     );
